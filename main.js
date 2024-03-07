ab7de57a8b891a9cf8a07783f96d591e19dede73
Swastika Yadav <Swastikayadav5358@gmail.com>
2024-03-07 11:16:02 +0530

    added js file for script

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  
+  // Show the "Submitting..." message
+  document.getElementById('submitMessage').style.display = 'block';
+
+});
