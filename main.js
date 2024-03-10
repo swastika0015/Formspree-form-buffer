88a702d2edd9128b82bae0669d8e8e283831ec42
Swastika Yadav <Swastikayadav5358@gmail.com>
2024-03-10 10:55:13 +0530

    animation added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.getElementById('myForm').addEventListener('submit', function(event) {
   
   // Show the "Submitting..." message
-  document.getElementById('submitMessage').style.display = 'block';
+  document.getElementById('loadingSymbol').style.display = 'block';
 
 });
